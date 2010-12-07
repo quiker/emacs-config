e5c1ec02e87c976a8e2768a7a4b47b877bd51dd6
q3ker <q3ker.ak@gmail.com>
2010-12-07 15:15:45 +0200

    add command for open my default org file org-my

--- rc-defuns.el
+++ rc-defuns.el
@@ -89,3 +89,7 @@ Symbols matching the text at point are put first in the completion list."
     (call-interactively 'grep)
     (setq ack-history             grep-history
           ack-host-defaults-alist grep-host-defaults-alist)))
+
+(defun org-my()
+  (interactive)
+  (find-file "~/q3ker.org"))
